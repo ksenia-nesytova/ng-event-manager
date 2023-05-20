41d0804293f4b0c2edf8ca4adbbd42262c815046
Ksenia Nesytova <passedbyabyss@gmail.com>
2023-05-20 18:36:28 +0700

    feat: shopping cart now displays added items

--- src/app/shopping-cart/shopping-cart.component.ts
+++ src/app/shopping-cart/shopping-cart.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-
+import { CartService } from '../cart.service';
+import { Product } from '../interfaces/Product'
 @Component({
   selector: 'app-shopping-cart',
   templateUrl: './shopping-cart.component.html',
   styleUrls: ['./shopping-cart.component.css']
 })
 export class ShoppingCartComponent {
+  constructor(public cartService: CartService) { }
+
+  items:Product[] = [];
+  ngOnInit() {
+   this.items = this.cartService.getItems();
+ }
 
 }
