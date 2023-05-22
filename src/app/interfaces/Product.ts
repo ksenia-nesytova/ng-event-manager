f0fd8f87d270b24c0a6c782e4b786c69288bf476
Ksenia Nesytova <passedbyabyss@gmail.com>
2023-05-22 22:31:36 +0700

    upd: combined Product & ProductInCart interfaces

--- src/app/interfaces/Product.ts
+++ src/app/interfaces/Product.ts
@@ -2,5 +2,11 @@ export interface Product {
   id: number,
   title: string,
   description: string,
-  vendor: string
+  vendor: string,
+  quantity?: number,
 }
+
+
+// export interface ProductInCart extends Product {
+//   quantity: number
+// }
