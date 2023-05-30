9e821464c956174cc7ce3727eb4904dbd1909184
Ksenia Nesytova <passedbyabyss@gmail.com>
2023-05-30 23:38:19 +0700

    add: product listing is now starting page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ProductListingComponent } from './product-listing/product-listing.component';
 
 
 const routes: Routes = [
+  { path: '', component: ProductListingComponent },
   { path: 'cart', component: ShoppingCartComponent },
   { path: 'product', component: ProductDetailsComponent }
 ];
