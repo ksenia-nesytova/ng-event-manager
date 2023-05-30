d5e0ee63189fd531a24bffc00599cd0ba48ec80f
Ksenia Nesytova <passedbyabyss@gmail.com>
2023-05-30 23:30:14 +0700

    add: defined routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+
+
+const routes: Routes = [
+  { path: 'cart', component: ShoppingCartComponent },
+  { path: 'product', component: ProductDetailsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
